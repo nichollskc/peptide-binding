e06cec329ae2eaf1c7d79a2830348d06d08f9738
Kath Nicholls <nichollskc@gmail.com>
2019-05-28 16:09:55 +0100

    Remove stash from pre-commit hook
    It wasn't reliable and sometimes ended up discardind changes.

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,10 +2,6 @@
 # pre-commit.sh
 # Runs tests and returns non-zero exit code if they failed
 
-# Stash any changes to make sure they don't interfere with tests
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index $STASH_NAME
-
 # Test prospective commit
 ./run_tests.sh
 TEST_RESULT=$?
@@ -13,12 +9,6 @@ TEST_RESULT=$?
 # Run pylint on all .py files and check no errors
 find . -iname "*.py" | xargs pylint --rcfile=.pylintrc
 PYLINT_RESULT=$?
-
-# Pop the stashed changes
-STASHES=$(git stash list)
-if [[ $STASHES == "$STASH_NAME" ]]; then
-  git stash pop -q
-fi
 
 # If either test failed, exit with code 1 so commit is halted
 [ $TEST_RESULT -ne 0 ] || [ $PYLINT_RESULT -ne 0 ] && exit 1
