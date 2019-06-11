e8dfb03701cfa19ecf5882f13451a966e3e37f52
Kath Nicholls <nichollskc@gmail.com>
2019-06-11 09:32:18 +0100

    Add snakemake dag creation to pre-push test
    - Checks snakemake pipeline fully connected

--- tests/pre-push.sh
+++ tests/pre-push.sh
@@ -13,6 +13,9 @@ TEST_RESULT=$?
 coverage report --fail-under=80
 COVERAGE_RESULT=$?
 
-# If either test failed, exit with code 1 so commit is halted
-[ $TEST_RESULT -ne 0 ] || [ $COVERAGE_RESULT -ne 0 ] || [ $PYLINT_RESULT -ne 0 ] && exit 1
+KCN_CURRENT_DIR=$(pwd) snakemake --dag | dot -Tpdf > dag.pdf
+SNAKEMAKE_RESULT=$?
+
+# If any test failed, exit with code 1 so commit is halted
+[ $SNAKEMAKE_RESULT -ne 0 ] || [ $TEST_RESULT -ne 0 ] || [ $COVERAGE_RESULT -ne 0 ] || [ $PYLINT_RESULT -ne 0 ] && exit 1
 exit 0
